2720a8b3192bfc9a8d4447051e09d07f60431fa2
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-07-21 15:37:39 +0800

    Delete artteam directory

--- artteam/js/logohtml.js
+++
@@ -1,9 +0,0 @@
-// logo页面
-document.addEventListener("DOMContentLoaded", () => {
-  const wrappers = document.querySelectorAll(".image-wrapper");
-  wrappers.forEach((wrapper, index) => {
-    setTimeout(() => {
-      wrapper.classList.add("fade-in");
-    }, index * 200); // 每个图片相隔200ms依次出现
-  });
-});
